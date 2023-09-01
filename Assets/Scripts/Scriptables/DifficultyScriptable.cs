1b845e5e5a02902313eb0d6e0910b087bbb60b1f
Rom.Supanut <51392680+romandrampc@users.noreply.github.com>
2023-09-01 19:11:04 +0700

    add context load img

--- Assets/Scripts/Scriptables/DifficultyScriptable.cs
+++ Assets/Scripts/Scriptables/DifficultyScriptable.cs
@@ -8,15 +8,13 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/DifficultyScriptable", order = 2)]
 public class DifficultyScriptable : SerializedScriptableObject
 {
-
-    [ContextMenuItem("Load Image", "LoadImg")]
     [Header("Load Img Config")]
-    [SerializeField] string type;
+    [SerializeField] string category;
     [SerializeField] string key;
-    [SerializeField] string number;
+    [SerializeField] int number;
     [Space(5)]
     [Header("Stage Config")]
-    public Dictionary<string,StageConfig> stageConfig = new Dictionary<string, StageConfig>();
+    public Dictionary<string, StageConfig> stageConfig = new Dictionary<string, StageConfig>();
     [HideInInspector]
     public List<string> easyID = new List<string>();
     [HideInInspector]
@@ -26,16 +24,46 @@ public class DifficultyScriptable : SerializedScriptableObject
     [HideInInspector]
     public List<string> advanceID = new List<string>();
 
+    [ContextMenu("Load All Image")]
+    public void LoadAllImg()
+    {
+        for (int i = 1; i < number; i++)
+        {
+            CreateDic(i);
+        }
+    }
+    [ContextMenu("Load  Image")]
     public void LoadImg()
     {
-        var stageConfig = new StageConfig();
-        string easyPath = $"Assets/Texture/{type}/1_Easy/{key}";
+        CreateDic(number);
+    }
+
+    private void CreateDic(int number)
+    {
+        var Config = new StageConfig();
+
+        string easyPath = $"Assets/Texture/{category}/1_Easy/{key}_E_{number:0000}.png";
         var easySprite = (Sprite)AssetDatabase.LoadAssetAtPath(easyPath, typeof(Sprite));
+        Config.easySprite = easySprite;
+
+        string normalPath = $"Assets/Texture/{category}/2_Normal/{key}_N_{number:0000}.png";
+        var normalSprite = (Sprite)AssetDatabase.LoadAssetAtPath(normalPath, typeof(Sprite));
+        Config.normalSprite = normalSprite;
+
+        string hardPath = $"Assets/Texture/{category}/3_Hard/{key}_H_{number:0000}.png";
+        var hardSprite = (Sprite)AssetDatabase.LoadAssetAtPath(hardPath, typeof(Sprite));
+        Config.hardSprite = hardSprite;
+
+        string advancePath = $"Assets/Texture/{category}/3_Hard/{key}_A_{number:0000}.png";
+        var advanceSprite = (Sprite)AssetDatabase.LoadAssetAtPath(advancePath, typeof(Sprite));
+        Config.advanceSprite = advanceSprite;
+
+        stageConfig.Add($"{key}_{number:0000}", Config);
     }
 
     public void SetObjValue()
     {
-        foreach (KeyValuePair<string,StageConfig> obj in stageConfig)
+        foreach (KeyValuePair<string, StageConfig> obj in stageConfig)
         {
             if (obj.Value.easySprite != null)
             {
