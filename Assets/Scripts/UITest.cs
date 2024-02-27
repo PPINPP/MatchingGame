3c97938cf3c9f7046dfd83a4643ccf2565a1bd6b
PRAWPLOY PARAYAPHODET <99686064+prawpho@users.noreply.github.com>
2024-02-27 21:41:51 +0700

    edit UITest code

--- Assets/Scripts/UITest.cs
+++ Assets/Scripts/UITest.cs
@@ -10,15 +10,12 @@ public class UITest : MonoBehaviour
 {
     // Start is called before the first frame update
     //public GameObject intoToTest;
-    public GameObject TestUI_Q01;
+    public GameObject TestUI_Q01, TestUI_Q02, TestUI_Q03, TestUI_Q04, TestUI_Q05, TestUI_Q06;
     public GameObject intoClick;
     public GameObject background;
+    private string question;
     //private bool _stateChange = false;
     [SerializeField] Dictionary<string, string> qAPairs= new Dictionary<string, string>();
-    //[SerializeField] DictAnswerPair newAnswer;
-    
-    
-    //[SerializeField] QAPair[] qAPairs;
 
     [Serializable]
     public class QAPair 
@@ -27,66 +24,80 @@ public class UITest : MonoBehaviour
         [SerializeField] private GameObject Answer;
         
     }
-
-    /*public class DictAnswerPair{
-        [SerializeField] QAPair[] qAPairs;
-        public Dictionary<string, string> ToDictionary()
-        {
-            Dictionary<string,string> UIAnswer = new Dictionary<string, string>();
-            foreach (var qAPair in qAPairs)
-            {
-                UIAnswer.Add(qAPair, qAPair);
-            }
-            return UIAnswer;
-        } 
-    }  */
     public void SwitchUITest() 
     {
         //intoToTest.GetComponent<Image>().sprite = Test;
         Debug.Log("SwitchUITest button pressed");
         Debug.Log("CheckName: "+background.name);
-        background.SetActive(false);
+        //background.SetActive(false);
         TestUI_Q01.SetActive(true);
         intoClick.SetActive(false);
+        Debug.Log(TestUI_Q01.activeSelf);
+        Debug.Log(TestUI_Q01.activeInHierarchy);
         //_stateChange = true;
 
     }
 
     public void CollectScore()
     {
-        string question = EventSystem.current.currentSelectedGameObject.name;
+        //string question;
         string answer = EventSystem.current.currentSelectedGameObject.name;
 
-        Debug.Log("Question button pressed" + question);
-        Debug.Log("Answer button pressed" + answer);
+        if (TestUI_Q01.activeInHierarchy==true){
+            TestUI_Q01.SetActive(false);
+            TestUI_Q02.SetActive(true);
+            question = "Q01";
+        }
+        else if (TestUI_Q02.activeInHierarchy==true){
+            TestUI_Q02.SetActive(false);
+            TestUI_Q03.SetActive(true);
+            question = "Q02";
+        }
+        else if (TestUI_Q03.activeInHierarchy==true){
+            TestUI_Q03.SetActive(false);
+            TestUI_Q04.SetActive(true);
+            question = "Q03";
+        }
+        else if (TestUI_Q04.activeInHierarchy==true){
+            TestUI_Q04.SetActive(false);
+            TestUI_Q05.SetActive(true);
+            question = "Q04";
+        }
+        else if (TestUI_Q05.activeInHierarchy==true){
+            TestUI_Q05.SetActive(false);
+            TestUI_Q06.SetActive(true);
+            question = "Q05";
+        }
+        else if (TestUI_Q06.activeInHierarchy==true){
+            TestUI_Q06.SetActive(false);
+            intoClick.SetActive(true);
+            question = "Q06";
+        }
+        else{
+            Debug.Log("No active UI");
+            Debug.Log("All answers collected: "+qAPairs);
+        }
+        
+
+        Debug.Log("Question button pressed: " + question);
+        Debug.Log("Answer button pressed: " + answer);
         qAPairs.Add(question, answer);
 
     }
 
     void Start()
     {
+        TestUI_Q01.SetActive(false);
+        TestUI_Q02.SetActive(false);
+        TestUI_Q03.SetActive(false);
+        TestUI_Q04.SetActive(false);
+        TestUI_Q05.SetActive(false);
+        TestUI_Q06.SetActive(false);
         foreach (var qAPair in qAPairs)
             {
                 Debug.Log("QAPair: " + qAPairs );
             }
     }
-
-        /*void Update()
-        {
-        if (Input.GetMouseButtonDown(0))
-            {
-                //empty RaycastHit object which raycast puts the hit details into
-                var hit : RaycastHit;
-                //ray shooting out of the camera from where the mouse is
-                var ray : Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-                if (Physics.Raycast(ray, hit))
-                {
-                    //print out the name if the raycast hits something
-                    Debug.Log(hit.collider.name);
-                }
-            }
-        }*/
 
 
     
