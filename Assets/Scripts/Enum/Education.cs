e6f8a27639bcdcd99cc5769e02945c1dd8b0efe1
Weerawat <weerawat190943@gmail.com>
2024-03-14 23:09:25 +0700

    init data manager and model

---
+++ Assets/Scripts/Enum/Education.cs
@@ -0,0 +1,12 @@
+namespace Enum
+{
+  public enum Educational
+  {
+    PRIMARY_OR_LOWER,
+    SECONDARY,
+    BACHELOR,
+    MASTER,
+    DOCTORAL,
+    TECHNICAL
+  }
+}
---
+++ Assets/Scripts/Enum/Gender.cs
@@ -0,0 +1,8 @@
+namespace Enum
+{
+  public enum Genders
+  {
+    MALE,
+    FEMALE
+  }
+}
