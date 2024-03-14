5c957b91e6ae0f12b7b0c35ededa77b6da0a8fef
Chalothorn Yooyuen <chalothorn.yn@gmail.com>
2024-03-15 00:49:58 +0700

    Complete UI Test flow

--- Assets/Scripts/UiTestManager.cs
+++ Assets/Scripts/UiTestManager.cs
@@ -1,21 +1,63 @@
-﻿using System.Collections;
+﻿using Sirenix.OdinInspector;
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Assets.Scripts
 {
     public class UiTestManager : MonoBehaviour
     {
+        [Title("Main Menu")]
+        [SerializeField] private GameObject mainMenu;
+        [SerializeField] private Button startBtn;
+
+        [Title("Questions")]
+        [SerializeField] private GameObject questionsHolder;
+        [SerializeField] List<Question> questionsList;
+        private int questionLenght = 0;
+        private int currQuestionIdx = 0;
 
         // Use this for initialization
         void Start()
         {
-
+            questionLenght = questionsList.Count;
+            Debug.Log("test");
+            startBtn.onClick.AddListener(() =>
+            {
+                Debug.Log("Start asking questions");
+                mainMenu.SetActive(false);
+                questionsHolder.SetActive(true);
+                initiateQuestion(currQuestionIdx);
+            });
         }
 
-        // Update is called once per frame
-        void Update()
+        private void initiateQuestion(int questionIdx) 
         {
+            Question question = questionsList[questionIdx];
 
+            question.gameObject.SetActive(true);
+            question.correctAnswer.onClick.AddListener(answer);
         }
+
+        private void answer() 
+        {
+            if(currQuestionIdx >= questionLenght) 
+            {
+                Debug.Log("Finish all Questions! congrat");
+                return;
+            }
+
+            questionsList[currQuestionIdx].gameObject.SetActive(false);
+            initiateQuestion(++currQuestionIdx);
+        }
+    }
+
+    [Serializable]
+    public struct Question
+    {
+        public GameObject gameObject;
+        public Button correctAnswer;
     }
 }
