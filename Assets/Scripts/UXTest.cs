ebb776ca880c427ba7fc04c268a2f158cf178144
Weerawat <weerawat190943@gmail.com>
2024-03-18 00:33:18 +0700

    Mapping data from ui to data manager

--- Assets/Scripts/UXTest.cs
+++ Assets/Scripts/UXTest.cs
@@ -1,142 +1,93 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
+using Manager;
+using Model;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
-using Firebase.Firestore;
-using Firebase.Extensions;
-using Firebase.Auth;
 
 public class UXTest : MonoBehaviour
 {
-    public GameObject UXtest_Title, UXtest_Q1, UXtest_Q2, UXtest_Q3;
-    private bool globalDebugMode = true;
-    float startTime;
-    int totalClick = 0, currentTask = 0;
+  public GameObject UXtest_Title, UXtest_Q1, UXtest_Q2, UXtest_Q3;
+  DateTime startedAt;
+  int totalClick = 0, currentTask = 0;
 
-    FirebaseFirestore db;
-    Firebase.Auth.FirebaseAuth auth;
-    Firebase.Auth.FirebaseUser loginuser;
+  List<UxClickLog> uxClickLogs = new List<UxClickLog>();
 
-    Dictionary<string, object> sessionData = new Dictionary<string, object>()
+  DataManager dataManager;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    dataManager = DataManager.Instance;
+  }
+
+  void Update()
+  {
+    if (Input.GetMouseButtonDown(0))
     {
-        {"totalClicks", 0},
-        {"totalTime", 0 },
-    };
-
-// Start is called before the first frame update
-    void Start()
-    { 
-        StartCoroutine(waiter());
-        (db, auth) = initialize(globalDebugMode);
-        loginuser = getUser(auth, globalDebugMode);
+      totalClick++;
+      uxClickLogs.Add(new UxClickLog(Input.mousePosition.x, Input.mousePosition.y, DateTime.Now, Enum.UxClickStatusEnum.MISS));
     }
+  }
 
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            totalClick = totalClick + 1;
-            sessionData.Add($"Click{totalClick}_taskNum", currentTask);
-            sessionData.Add($"Click{totalClick}_location", $"{Input.mousePosition.x},{Input.mousePosition.y}");
-            sessionData.Add($"Click{totalClick}_timestamp", Time.time);
-        }
-    }
 
-    IEnumerator waiter()
-    {
-        yield return new WaitForSeconds(3);
-        toUXtest_Q1();
-    }
 
-    public void toUXtest_Q1()
-    {
-        startTime = Time.time;
-        currentTask = 1;
-        UXtest_Q1.SetActive(true);
-        UXtest_Q2.SetActive(false);
-        UXtest_Q3.SetActive(false);
-        UXtest_Title.SetActive(false);
-    }
+  public void ToUXtest_Q1()
+  {
+    startedAt = DateTime.Now;
+    currentTask = 1;
+    UXtest_Q1.SetActive(true);
+    UXtest_Q2.SetActive(false);
+    UXtest_Q3.SetActive(false);
+    UXtest_Title.SetActive(false);
+  }
 
-    public void toUXtest_Q2()
-    {
-        currentTask = 2;
-        UXtest_Q2.SetActive(true); 
-        UXtest_Q3.SetActive(false); 
-        UXtest_Title.SetActive(false);
-        UXtest_Q1.SetActive(false);
-    }
+  public void ToUXtest_Q2()
+  {
+    uxClickLogs[^1].ClickStatus = Enum.UxClickStatusEnum.HIT;
+    DateTime completedAt = DateTime.Now;
+    float timeUsed = (float)completedAt.Subtract(startedAt).TotalSeconds;
+    UxTestResult uxTestResult = new UxTestResult(totalClick, startedAt, completedAt, timeUsed, new List<UxClickLog>(uxClickLogs));
+    dataManager.UxTestResultList.Add(uxTestResult);
 
-    public void toUXtest_Q3()
-    {
-        currentTask = 3;
-        UXtest_Q3.SetActive(true);
-        UXtest_Q1.SetActive(false);
-        UXtest_Q2.SetActive(false);
-        UXtest_Title.SetActive(false);
-    }
+    totalClick = 0;
+    startedAt = DateTime.Now;
+    uxClickLogs = new List<UxClickLog>();
+    currentTask = 2;
+    UXtest_Q2.SetActive(true);
+    UXtest_Q3.SetActive(false);
+    UXtest_Title.SetActive(false);
+    UXtest_Q1.SetActive(false);
+  }
 
-    public void finishTest(string target)
-    {
-        sessionData["totalTime"] = Time.time - startTime;
-        Debug.Log($"Time taken {sessionData["totalTime"]}");
+  public void ToUXtest_Q3()
+  {
+    uxClickLogs[^1].ClickStatus = Enum.UxClickStatusEnum.HIT;
+    DateTime completedAt = DateTime.Now;
+    float timeUsed = (float)completedAt.Subtract(startedAt).TotalSeconds;
+    UxTestResult uxTestResult = new UxTestResult(totalClick, startedAt, completedAt, timeUsed, new List<UxClickLog>(uxClickLogs));
+    dataManager.UxTestResultList.Add(uxTestResult);
 
-        sessionData["totalClicks"] = totalClick;
-        Debug.Log($"Total clicks: {sessionData["totalClicks"]}");
+    totalClick = 0;
+    startedAt = DateTime.Now;
+    uxClickLogs = new List<UxClickLog>();
+    startedAt = DateTime.Now;
+    currentTask = 3;
+    UXtest_Q3.SetActive(true);
+    UXtest_Q1.SetActive(false);
+    UXtest_Q2.SetActive(false);
+    UXtest_Title.SetActive(false);
+  }
 
-        Debug.Log($"Session data: {sessionData}");
-        foreach(var kvp in  sessionData)
-            Debug.Log($"Key: {kvp.Key}, Value: {kvp.Value}");
+  public void FinishTest(string target)
+  {
+    uxClickLogs[^1].ClickStatus = Enum.UxClickStatusEnum.HIT;
+    DateTime completedAt = DateTime.Now;
+    float timeUsed = (float)completedAt.Subtract(startedAt).TotalSeconds;
+    UxTestResult uxTestResult = new UxTestResult(totalClick, startedAt, completedAt, timeUsed, new List<UxClickLog>(uxClickLogs));
+    dataManager.UxTestResultList.Add(uxTestResult);
 
-        // Push to DB
-        DocumentReference docref = db.Collection("GamePlayHistory").Document(loginuser.DisplayName);
-        Dictionary<string, Dictionary<string, object>> clickData = new Dictionary<string, Dictionary<string, object>>()
-        {
-            { "ClickData", sessionData }
-        };
-        docref.SetAsync(clickData, SetOptions.MergeAll).ContinueWithOnMainThread(task =>
-        {
-            Debug.Log("Added data to db successfully.");
-        });
-        // Finish push to DB
-
-        SceneManager.LoadScene(target);
-    }
-
-    private (FirebaseFirestore, Firebase.Auth.FirebaseAuth) initialize(bool debugMode = false)
-    {
-        FirebaseFirestore db = FirebaseFirestore.DefaultInstance;
-        Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
-
-        if (debugMode)
-        {
-            Debug.Log($"Firestore object: {db}");
-            Debug.Log($"Firebase Auth object: {auth}");
-        }
-
-        return (db, auth);
-    }
-
-    private Firebase.Auth.FirebaseUser getUser(Firebase.Auth.FirebaseAuth auth, bool debugMode = false)
-    {
-        Firebase.Auth.FirebaseUser loginuser = auth.CurrentUser;
-        if (debugMode)
-        {
-            if (loginuser != null)
-            {
-                string name = loginuser.DisplayName;
-                string email = loginuser.Email;
-                string uid = loginuser.UserId;
-                Debug.Log($"Username {name}, Email: {email}, Uid: {uid}");
-            }
-            else
-            {
-                Debug.Log("User not found!");
-            }
-        }
-
-        return loginuser;
-    }
+    SceneManager.LoadScene(target);
+  }
 
 }
