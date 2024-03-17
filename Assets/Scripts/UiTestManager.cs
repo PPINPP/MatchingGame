c15a01b553e6ddb6dab314f1c5162c2bbeeb2245
Chalothorn Yooyuen <chalothorn.yn@gmail.com>
2024-03-17 13:40:26 +0700

    Create UiTestResult object

--- Assets/Scripts/UiTestManager.cs
+++ Assets/Scripts/UiTestManager.cs
@@ -22,50 +22,67 @@ namespace Assets.Scripts
 
         private List<UiTestResult> uiTestResultsList;
 
+        private DateTime startedAt = new DateTime(1999,1,1);
+        private DateTime completedAt = new DateTime(1999, 1, 1);
+
         // Use this for initialization
         void Start()
         {
+            mainMenu.SetActive(true);
+            questionsHolder.SetActive(false);
+
             questionLenght = questionsList.Count;
+
             startBtn.onClick.AddListener(() =>
             {
                 Debug.Log("Start asking questions");
                 mainMenu.SetActive(false);
                 questionsHolder.SetActive(true);
-                initiateQuestion(currQuestionIdx);
+                initiateNextQuestion(currQuestionIdx);
             });
-            uiTestResultsList = new List<UiTestResult>();
 
-            for (int i = 0; i < questionLenght; i++)
-            {
-                uiTestResultsList.Add(new UiTestResult());
-            }
+            uiTestResultsList = new List<UiTestResult>();
         }
 
-        private void initiateQuestion(int questionIdx) 
+        private void initiateNextQuestion(int idx) 
         {
-            Question question = questionsList[questionIdx];
+            Question question = questionsList[idx];
 
-            question.gameObject.SetActive(true);
             question.correctAnswer.onClick.AddListener(answer);
+            question.gameObject.SetActive(true);
+
+            startedAt = DateTime.Now;
         }
 
         private void answer() 
         {
-            if(currQuestionIdx >= questionLenght) 
+            completedAt = DateTime.Now;
+            float timeUsed = (float)completedAt.Subtract(startedAt).TotalSeconds;
+
+            uiTestResultsList.Add(new UiTestResult(
+                    questionsList[currQuestionIdx].textAnswer,
+                    DateTime.Parse(startedAt.ToString()),
+                    DateTime.Parse(completedAt.ToString()),
+                    timeUsed
+                ));
+
+            startedAt = new DateTime(1999, 1, 1);
+            completedAt = new DateTime(1999, 1, 1);
+
+            currQuestionIdx = currQuestionIdx + 1;
+
+            if (currQuestionIdx >= questionLenght)
             {
                 Debug.Log("Finish all Questions! congrat");
+                foreach (var e in uiTestResultsList)
+                {
+                    Debug.Log($"{e.SelectedChoice} {e.StartedAt} {e.CompletedAt} {e.TimeUsed}");
+                }
                 return;
             }
 
-            UiTestResult result = new UiTestResult(
-                    "test",
-                    DateTime.Now,
-                    DateTime.Now,
-                    10f
-                );
-
             questionsList[currQuestionIdx].gameObject.SetActive(false);
-            initiateQuestion(++currQuestionIdx);
+            initiateNextQuestion(currQuestionIdx);
         }
     }
 
