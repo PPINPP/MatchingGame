e70f4ef7be9087e588108500def59b93d3b674ce
Weerawat <weerawat190943@gmail.com>
2024-03-18 23:52:16 +0700

    Integrate flow with register, Ui Test and Ux test then push data to firebase

--- Assets/Scripts/Manager/DataManager.cs
+++ Assets/Scripts/Manager/DataManager.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Firebase.Firestore;
 using Model;
 
 namespace Manager
@@ -8,5 +9,24 @@ namespace Manager
     public UserInfo UserInfo { get; set; } = new UserInfo();
     public List<UxTestResult> UxTestResultList { get; set; } = new List<UxTestResult>();
     public List<UiTestResult> UiTestResultList { get; set; } = new List<UiTestResult>();
+
+
+    public void PushDataToFirebase()
+    {
+      //TODO Create Firebase account
+
+      _ = FirebaseManager.Instance.CreateDataWithDoc(UserInfo.Username, $"UserInfo", UserInfo.ConvertUserInfoToUserInfoFs(), SetOptions.Overwrite);
+
+      for (int i = 0; i < UiTestResultList.Count; i++)
+      {
+        _ = FirebaseManager.Instance.CreateDataWithDoc(UserInfo.Username, $"DemoResult/UI Test/task{i:D2}", UiTestResultList[i].ConvertUiTestResultToUiTestResultFs(), SetOptions.Overwrite);
+      }
+
+      for (int i = 0; i < UxTestResultList.Count; i++)
+      {
+        _ = FirebaseManager.Instance.CreateDataWithDoc(UserInfo.Username, $"DemoResult/UX Test/task{i:D2}", UxTestResultList[i].ConvertUxTestResultToUxTestResultFs(), SetOptions.Overwrite);
+      }
+
+    }
   }
 }
--- Assets/Scripts/Register & Login/Register02DataManager.cs
+++ Assets/Scripts/Register & Login/Register02DataManager.cs
@@ -3,44 +3,47 @@ using UnityEngine.SceneManagement;
 using Enum;
 using Manager;
 
-public class Register02DataManager : MonoBehaviour
+namespace Register
 {
-  public GameObject primButton, highsButton, certButton, bacButton, masButton, phdButton;
-  public string nextScene;
-  EducationalEnum educationalLevel;
+  public class Register02DataManager : MonoBehaviour
+  {
+    public GameObject primButton, highsButton, certButton, bacButton, masButton, phdButton;
+    public string nextScene;
+    EducationalEnum educationalLevel;
 
 
-  public void SubmitForm()
-  {
-    Debug.Log($"Educational Select: {educationalLevel}");
+    public void SubmitForm()
+    {
+      Debug.Log($"Educational Select: {educationalLevel}");
 
-    DataManager.Instance.UserInfo.EducationalLevel = educationalLevel;
+      DataManager.Instance.UserInfo.EducationalLevel = educationalLevel;
 
-    SceneManager.LoadScene(nextScene);
-  }
+      SceneManager.LoadScene(nextScene);
+    }
 
-  public void primSelected()
-  {
-    educationalLevel = EducationalEnum.PRIMARY_OR_LOWER;
-  }
-  public void highsSelected()
-  {
-    educationalLevel = EducationalEnum.HIGH_SCHOOL;
-  }
-  public void certSelected()
-  {
-    educationalLevel = EducationalEnum.TECHNICAL;
-  }
-  public void bacSelected()
-  {
-    educationalLevel = EducationalEnum.BACHELOR_DEGREE;
-  }
-  public void masSelected()
-  {
-    educationalLevel = EducationalEnum.MASTER_DEGREE;
-  }
-  public void phdSelected()
-  {
-    educationalLevel = EducationalEnum.DOCTORAL;
+    public void primSelected()
+    {
+      educationalLevel = EducationalEnum.PRIMARY_OR_LOWER;
+    }
+    public void highsSelected()
+    {
+      educationalLevel = EducationalEnum.HIGH_SCHOOL;
+    }
+    public void certSelected()
+    {
+      educationalLevel = EducationalEnum.TECHNICAL;
+    }
+    public void bacSelected()
+    {
+      educationalLevel = EducationalEnum.BACHELOR_DEGREE;
+    }
+    public void masSelected()
+    {
+      educationalLevel = EducationalEnum.MASTER_DEGREE;
+    }
+    public void phdSelected()
+    {
+      educationalLevel = EducationalEnum.DOCTORAL;
+    }
   }
 }
