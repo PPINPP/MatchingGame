be85693431783988358e39a074e4f39860808fe8
Weerawat <weerawat190943@gmail.com>
2024-03-18 23:52:16 +0700

    Integrate flow with register, Ui Test and Ux test then push data to firebase

--- Assets/Scripts/UXTest.cs
+++ Assets/Scripts/UXTest.cs
@@ -5,89 +5,88 @@ using Model;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class UXTest : MonoBehaviour
+namespace UxTest
 {
-  public GameObject UXtest_Title, UXtest_Q1, UXtest_Q2, UXtest_Q3;
-  DateTime startedAt;
-  int totalClick = 0, currentTask = 0;
-
-  List<UxClickLog> uxClickLogs = new List<UxClickLog>();
-
-  DataManager dataManager;
-
-  // Start is called before the first frame update
-  void Start()
+  public class UXTest : MonoBehaviour
   {
-    dataManager = DataManager.Instance;
-  }
+    public GameObject UXtest_Title, UXtest_Q1, UXtest_Q2, UXtest_Q3;
+    DateTime startedAt;
+    int totalClick = 0;
 
-  void Update()
-  {
-    if (Input.GetMouseButtonDown(0))
+    List<UxClickLog> uxClickLogs = new List<UxClickLog>();
+
+    DataManager dataManager;
+
+    // Start is called before the first frame update
+    void Start()
     {
-      totalClick++;
-      uxClickLogs.Add(new UxClickLog(Input.mousePosition.x, Input.mousePosition.y, DateTime.Now, Enum.UxClickStatusEnum.MISS));
+      dataManager = DataManager.Instance;
+    }
+
+    void Update()
+    {
+      if (Input.GetMouseButtonDown(0))
+      {
+        totalClick++;
+        uxClickLogs.Add(new UxClickLog(Input.mousePosition.x, Input.mousePosition.y, DateTime.Now, Enum.UxClickStatusEnum.MISS));
+      }
+    }
+
+
+
+    public void ToUXtest_Q1()
+    {
+      startedAt = DateTime.Now;
+      UXtest_Q1.SetActive(true);
+      UXtest_Q2.SetActive(false);
+      UXtest_Q3.SetActive(false);
+      UXtest_Title.SetActive(false);
+    }
+
+    public void ToUXtest_Q2()
+    {
+      uxClickLogs[^1].ClickStatus = Enum.UxClickStatusEnum.HIT;
+      DateTime completedAt = DateTime.Now;
+      float timeUsed = (float)completedAt.Subtract(startedAt).TotalSeconds;
+      UxTestResult uxTestResult = new UxTestResult(totalClick, startedAt, completedAt, timeUsed, new List<UxClickLog>(uxClickLogs));
+      dataManager.UxTestResultList.Add(uxTestResult);
+
+      totalClick = 0;
+      startedAt = DateTime.Now;
+      uxClickLogs = new List<UxClickLog>();
+      UXtest_Q2.SetActive(true);
+      UXtest_Q3.SetActive(false);
+      UXtest_Title.SetActive(false);
+      UXtest_Q1.SetActive(false);
+    }
+
+    public void ToUXtest_Q3()
+    {
+      uxClickLogs[^1].ClickStatus = Enum.UxClickStatusEnum.HIT;
+      DateTime completedAt = DateTime.Now;
+      float timeUsed = (float)completedAt.Subtract(startedAt).TotalSeconds;
+      UxTestResult uxTestResult = new UxTestResult(totalClick, startedAt, completedAt, timeUsed, new List<UxClickLog>(uxClickLogs));
+      dataManager.UxTestResultList.Add(uxTestResult);
+
+      totalClick = 0;
+      startedAt = DateTime.Now;
+      uxClickLogs = new List<UxClickLog>();
+      startedAt = DateTime.Now;
+      UXtest_Q3.SetActive(true);
+      UXtest_Q1.SetActive(false);
+      UXtest_Q2.SetActive(false);
+      UXtest_Title.SetActive(false);
+    }
+
+    public void FinishTest(string target)
+    {
+      uxClickLogs[^1].ClickStatus = Enum.UxClickStatusEnum.HIT;
+      DateTime completedAt = DateTime.Now;
+      float timeUsed = (float)completedAt.Subtract(startedAt).TotalSeconds;
+      UxTestResult uxTestResult = new UxTestResult(totalClick, startedAt, completedAt, timeUsed, new List<UxClickLog>(uxClickLogs));
+      dataManager.UxTestResultList.Add(uxTestResult);
+
+      SceneManager.LoadScene(target);
     }
   }
-
-
-
-  public void ToUXtest_Q1()
-  {
-    startedAt = DateTime.Now;
-    currentTask = 1;
-    UXtest_Q1.SetActive(true);
-    UXtest_Q2.SetActive(false);
-    UXtest_Q3.SetActive(false);
-    UXtest_Title.SetActive(false);
-  }
-
-  public void ToUXtest_Q2()
-  {
-    uxClickLogs[^1].ClickStatus = Enum.UxClickStatusEnum.HIT;
-    DateTime completedAt = DateTime.Now;
-    float timeUsed = (float)completedAt.Subtract(startedAt).TotalSeconds;
-    UxTestResult uxTestResult = new UxTestResult(totalClick, startedAt, completedAt, timeUsed, new List<UxClickLog>(uxClickLogs));
-    dataManager.UxTestResultList.Add(uxTestResult);
-
-    totalClick = 0;
-    startedAt = DateTime.Now;
-    uxClickLogs = new List<UxClickLog>();
-    currentTask = 2;
-    UXtest_Q2.SetActive(true);
-    UXtest_Q3.SetActive(false);
-    UXtest_Title.SetActive(false);
-    UXtest_Q1.SetActive(false);
-  }
-
-  public void ToUXtest_Q3()
-  {
-    uxClickLogs[^1].ClickStatus = Enum.UxClickStatusEnum.HIT;
-    DateTime completedAt = DateTime.Now;
-    float timeUsed = (float)completedAt.Subtract(startedAt).TotalSeconds;
-    UxTestResult uxTestResult = new UxTestResult(totalClick, startedAt, completedAt, timeUsed, new List<UxClickLog>(uxClickLogs));
-    dataManager.UxTestResultList.Add(uxTestResult);
-
-    totalClick = 0;
-    startedAt = DateTime.Now;
-    uxClickLogs = new List<UxClickLog>();
-    startedAt = DateTime.Now;
-    currentTask = 3;
-    UXtest_Q3.SetActive(true);
-    UXtest_Q1.SetActive(false);
-    UXtest_Q2.SetActive(false);
-    UXtest_Title.SetActive(false);
-  }
-
-  public void FinishTest(string target)
-  {
-    uxClickLogs[^1].ClickStatus = Enum.UxClickStatusEnum.HIT;
-    DateTime completedAt = DateTime.Now;
-    float timeUsed = (float)completedAt.Subtract(startedAt).TotalSeconds;
-    UxTestResult uxTestResult = new UxTestResult(totalClick, startedAt, completedAt, timeUsed, new List<UxClickLog>(uxClickLogs));
-    dataManager.UxTestResultList.Add(uxTestResult);
-
-    SceneManager.LoadScene(target);
-  }
-
 }
--- Assets/Scripts/UiTestManager.cs
+++ Assets/Scripts/UiTestManager.cs
@@ -2,90 +2,92 @@
 using Model;
 using Sirenix.OdinInspector;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace Assets.Scripts
+namespace UiTest
 {
-    public class UiTestManager : MonoBehaviour
+  public class UiTestManager : MonoBehaviour
+  {
+    [Title("Main Menu")]
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private Button startBtn;
+
+    [Title("Questions")]
+    [SerializeField] private GameObject questionsHolder;
+    [SerializeField] List<GameObject> questionsList;
+    private int questionLength = 0;
+    private int currQuestionIdx = 0;
+
+    private DateTime startedAt = new DateTime(1999, 1, 1);
+    private DateTime completedAt = new DateTime(1999, 1, 1);
+
+    // Use this for initialization
+    void Start()
     {
-        [Title("Main Menu")]
-        [SerializeField] private GameObject mainMenu;
-        [SerializeField] private Button startBtn;
+      mainMenu.SetActive(true);
+      questionsHolder.SetActive(false);
 
-        [Title("Questions")]
-        [SerializeField] private GameObject questionsHolder;
-        [SerializeField] List<GameObject> questionsList;
-        private int questionLength = 0;
-        private int currQuestionIdx = 0;
+      questionLength = questionsList.Count;
 
-        private DateTime startedAt = new DateTime(1999,1,1);
-        private DateTime completedAt = new DateTime(1999, 1, 1);
-
-        // Use this for initialization
-        void Start()
-        {
-            mainMenu.SetActive(true);
-            questionsHolder.SetActive(false);
-
-            questionLength = questionsList.Count;
-
-            startBtn.onClick.AddListener(() =>
-            {
-                Debug.Log("Start asking questions");
-                mainMenu.SetActive(false);
-                questionsHolder.SetActive(true);
-                initiateNextQuestion(currQuestionIdx);
-            });
-        }
-
-        private void initiateNextQuestion(int idx) 
-        {
-            GameObject question = questionsList[idx];
-
-            foreach (Button btn in question.GetComponentsInChildren<Button>())
-            {
-                btn.onClick.AddListener(() => answer(btn));
-            }
-
-            question.gameObject.SetActive(true);
-
-            startedAt = DateTime.Now;
-        }
-
-        private void answer(Button clickedBtn)
-        { 
-            completedAt = DateTime.Now;
-            float timeUsed = (float)completedAt.Subtract(startedAt).TotalSeconds;
-
-            DataManager.Instance.UiTestResultList.Add(
-                new UiTestResult(
-                    clickedBtn.name,
-                    DateTime.Parse(startedAt.ToString()),
-                    DateTime.Parse(completedAt.ToString()),
-                    timeUsed
-                )
-            );
-
-            startedAt = new DateTime(1999, 1, 1);
-            completedAt = new DateTime(1999, 1, 1);
-
-            currQuestionIdx = currQuestionIdx + 1;
-
-            if (currQuestionIdx >= questionLength)
-            {
-                Debug.Log("Finish all Questions! congrat");
-                foreach (var e in DataManager.Instance.UiTestResultList)
-                {
-                    Debug.Log($"{e.SelectedChoice} {e.StartedAt} {e.CompletedAt} {e.TimeUsed}");
-                }
-                return;
-            }
-
-            questionsList[currQuestionIdx].gameObject.SetActive(false);
-            initiateNextQuestion(currQuestionIdx);
-        }
+      startBtn.onClick.AddListener(() =>
+      {
+        Debug.Log("Start asking questions");
+        mainMenu.SetActive(false);
+        questionsHolder.SetActive(true);
+        initiateNextQuestion(currQuestionIdx);
+      });
     }
+
+    private void initiateNextQuestion(int idx)
+    {
+      GameObject question = questionsList[idx];
+
+      foreach (Button btn in question.GetComponentsInChildren<Button>())
+      {
+        btn.onClick.AddListener(() => answer(btn));
+      }
+
+      question.gameObject.SetActive(true);
+
+      startedAt = DateTime.Now;
+    }
+
+    private void answer(Button clickedBtn)
+    {
+      completedAt = DateTime.Now;
+      float timeUsed = (float)completedAt.Subtract(startedAt).TotalSeconds;
+
+      DataManager.Instance.UiTestResultList.Add(
+          new UiTestResult(
+              clickedBtn.name,
+              DateTime.Parse(startedAt.ToString()),
+              DateTime.Parse(completedAt.ToString()),
+              timeUsed
+          )
+      );
+
+      startedAt = new DateTime(1999, 1, 1);
+      completedAt = new DateTime(1999, 1, 1);
+
+      currQuestionIdx = currQuestionIdx + 1;
+
+      if (currQuestionIdx >= questionLength)
+      {
+        Debug.Log("Finish all Questions! congrat");
+        foreach (var e in DataManager.Instance.UiTestResultList)
+        {
+          Debug.Log($"{e.SelectedChoice} {e.StartedAt} {e.CompletedAt} {e.TimeUsed}");
+        }
+
+        DataManager.Instance.PushDataToFirebase();
+
+        return;
+      }
+
+      questionsList[currQuestionIdx].gameObject.SetActive(false);
+      initiateNextQuestion(currQuestionIdx);
+    }
+  }
 }
