4bba0f8bc1e12c306b48b776b3c412d3d11297ae
Chalothorn Yooyuen <chalothorn.yn@gmail.com>
2024-03-30 17:31:15 +0700

    Get gameobject name from selected toggle

--- Assets/Scripts/SmileyoMeterManager.cs
+++ Assets/Scripts/SmileyoMeterManager.cs
@@ -1,15 +1,43 @@
 ﻿using System.Collections;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Assets.Scripts
 {
     public class SmileyoMeterManager : MonoBehaviour
     {
+        [SerializeField] private ToggleGroup funnessToggleGrp;
+        [SerializeField] private ToggleGroup fatiguenessToggleGrp;
 
         // Use this for initialization
         void Start()
         {
+            funnessToggleGrp.SetAllTogglesOff();
+            fatiguenessToggleGrp.SetAllTogglesOff();
+        }
 
+        void Update()
+        {
+            if (funnessToggleGrp.AnyTogglesOn())
+            {
+                if (funnessToggleGrp.allowSwitchOff)
+                {
+                    funnessToggleGrp.allowSwitchOff = false;
+                }
+                    
+                Debug.Log(funnessToggleGrp.ActiveToggles().FirstOrDefault().name);
+            }
+
+            if (fatiguenessToggleGrp.AnyTogglesOn())
+            {
+                if (fatiguenessToggleGrp.allowSwitchOff)
+                {
+                    fatiguenessToggleGrp.allowSwitchOff = false;
+                }
+
+                Debug.Log(fatiguenessToggleGrp.ActiveToggles().FirstOrDefault().name);
+            }
         }
 
 
