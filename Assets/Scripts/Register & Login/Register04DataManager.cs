92db0e2d3534c09157bc61c58145ad9a430fbab6
Weerawat <weerawat190943@gmail.com>
2024-04-01 23:41:41 +0700

    fix date of birth error and less opa register 04 scene

--- Assets/Scripts/Register & Login/Register04DataManager.cs
+++ Assets/Scripts/Register & Login/Register04DataManager.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using Enum;
 using Manager;
+using UnityEngine.UI;
+using Constant;
 
 namespace Register
 {
@@ -10,6 +12,11 @@ namespace Register
     public GameObject noButton, mciButton, alzButton;
     public string nextScene;
     DementiaStageEnum dementiaStage;
+
+    private void Start()
+    {
+      noSelected();
+    }
 
     public void SubmitForm()
     {
@@ -23,16 +30,52 @@ namespace Register
     public void noSelected()
     {
       dementiaStage = DementiaStageEnum.HEALTHY;
+
+      var noButtonNormalColor = noButton.GetComponent<Button>().colors;
+      var mciButtonNormalColor = mciButton.GetComponent<Button>().colors;
+      var alzButtonNormalColor = alzButton.GetComponent<Button>().colors;
+
+      noButtonNormalColor.normalColor = new Color(1f, 1f, 1f, ButtonColorConstant.SELECTED_COLOR); ;
+      mciButtonNormalColor.normalColor = new Color(1f, 1f, 1f, ButtonColorConstant.UNSELECTED_COLOR); ;
+      alzButtonNormalColor.normalColor = new Color(1f, 1f, 1f, ButtonColorConstant.UNSELECTED_COLOR); ;
+
+      noButton.GetComponent<Button>().colors = noButtonNormalColor;
+      mciButton.GetComponent<Button>().colors = mciButtonNormalColor;
+      alzButton.GetComponent<Button>().colors = alzButtonNormalColor;
     }
 
     public void alzSelected()
     {
       dementiaStage = DementiaStageEnum.ALZHEIMER;
+
+      var noButtonNormalColor = noButton.GetComponent<Button>().colors;
+      var mciButtonNormalColor = mciButton.GetComponent<Button>().colors;
+      var alzButtonNormalColor = alzButton.GetComponent<Button>().colors;
+
+      noButtonNormalColor.normalColor = new Color(1f, 1f, 1f, ButtonColorConstant.UNSELECTED_COLOR); ;
+      mciButtonNormalColor.normalColor = new Color(1f, 1f, 1f, ButtonColorConstant.UNSELECTED_COLOR); ;
+      alzButtonNormalColor.normalColor = new Color(1f, 1f, 1f, ButtonColorConstant.SELECTED_COLOR); ;
+
+      noButton.GetComponent<Button>().colors = noButtonNormalColor;
+      mciButton.GetComponent<Button>().colors = mciButtonNormalColor;
+      alzButton.GetComponent<Button>().colors = alzButtonNormalColor;
     }
 
     public void mciSelected()
     {
       dementiaStage = DementiaStageEnum.MCI;
+
+      var noButtonNormalColor = noButton.GetComponent<Button>().colors;
+      var mciButtonNormalColor = mciButton.GetComponent<Button>().colors;
+      var alzButtonNormalColor = alzButton.GetComponent<Button>().colors;
+
+      noButtonNormalColor.normalColor = new Color(1f, 1f, 1f, ButtonColorConstant.UNSELECTED_COLOR); ;
+      mciButtonNormalColor.normalColor = new Color(1f, 1f, 1f, ButtonColorConstant.SELECTED_COLOR); ;
+      alzButtonNormalColor.normalColor = new Color(1f, 1f, 1f, ButtonColorConstant.UNSELECTED_COLOR); ;
+
+      noButton.GetComponent<Button>().colors = noButtonNormalColor;
+      mciButton.GetComponent<Button>().colors = mciButtonNormalColor;
+      alzButton.GetComponent<Button>().colors = alzButtonNormalColor;
     }
   }
 }
