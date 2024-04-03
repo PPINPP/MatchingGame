1a5094b28bd38c320ccf6fc8633de67c737b17a6
Chalothorn Yooyuen <chalothorn.yn@gmail.com>
2024-04-03 22:41:25 +0700

    Fix conflict and add submit fn

--- Assets/Scripts/SmileyoMeterManager.cs
+++ Assets/Scripts/SmileyoMeterManager.cs
@@ -1,65 +1,86 @@
-﻿using System.Collections;
+﻿using Manager;
+using Model;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 namespace Assets.Scripts
 {
-  public class SmileyoMeterManager : MonoBehaviour
-  {
-    [SerializeField] private ToggleGroup funnessToggleGrp;
-    [SerializeField] private ToggleGroup fatiguenessToggleGrp;
-
-    // Use this for initialization
-    void Start()
+    public class SmileyoMeterManager : MonoBehaviour
     {
-      funnessToggleGrp.SetAllTogglesOff();
-      fatiguenessToggleGrp.SetAllTogglesOff();
-    }
+        [SerializeField] private ToggleGroup funnessToggleGrp;
+        [SerializeField] private ToggleGroup fatiguenessToggleGrp;
+        public string targetScene;
 
-    void Update()
-    {
-      if (funnessToggleGrp.AnyTogglesOn())
-      {
-        if (funnessToggleGrp.allowSwitchOff)
+        // Use this for initialization
+        void Start()
         {
-          funnessToggleGrp.allowSwitchOff = false;
+            funnessToggleGrp.SetAllTogglesOff();
+            fatiguenessToggleGrp.SetAllTogglesOff();
         }
 
-        Debug.Log(funnessToggleGrp.ActiveToggles().FirstOrDefault().name);
-      }
-
-      if (fatiguenessToggleGrp.AnyTogglesOn())
-      {
-        if (fatiguenessToggleGrp.allowSwitchOff)
+        void Update()
         {
-          fatiguenessToggleGrp.allowSwitchOff = false;
+            if (funnessToggleGrp.AnyTogglesOn())
+            {
+                if (funnessToggleGrp.allowSwitchOff)
+                {
+                    funnessToggleGrp.allowSwitchOff = false;
+                }
+
+                Debug.Log(funnessToggleGrp.ActiveToggles().FirstOrDefault().name);
+            }
+
+            if (fatiguenessToggleGrp.AnyTogglesOn())
+            {
+                if (fatiguenessToggleGrp.allowSwitchOff)
+                {
+                    fatiguenessToggleGrp.allowSwitchOff = false;
+                }
+
+                Debug.Log(fatiguenessToggleGrp.ActiveToggles().FirstOrDefault().name);
+            }
         }
 
-        Debug.Log(fatiguenessToggleGrp.ActiveToggles().FirstOrDefault().name);
-      }
+        void SubmitForm()
+        {
+            if (!fatiguenessToggleGrp.AnyTogglesOn() || !funnessToggleGrp.AnyTogglesOn())
+            {
+                Debug.Log("Please answer correctly");
+                return;
+            }
+                
+            int enjoyable = MapToggleNameToNumber(fatiguenessToggleGrp.ActiveToggles().FirstOrDefault().name);
+            int fatigue = MapToggleNameToNumber(fatiguenessToggleGrp.ActiveToggles().FirstOrDefault().name);
+            
+            SmileyoMeterResult smileyoMeterResult = new(enjoyable, fatigue);
+
+            DataManager.Instance.SmileyoMeterResultList.Add(smileyoMeterResult);
+
+            Debug.Log("Save result");
+
+            SceneManager.LoadScene(targetScene);
+        }
+
+        int MapToggleNameToNumber(string toggleName)
+        {
+            switch (toggleName)
+            {
+                case "green_tgr":
+                    return 1;
+                case "l_green_tgr":
+                    return 2;
+                case "yellow_tgr":
+                    return 3;
+                case "orange_tgr":
+                    return 4;
+                case "red_tgr":
+                    return 5;
+                default:
+                    return 0;
+            }
+        }
+
     }
-
-
-
-    int MapToggleNameToNumber(string toggleName)
-    {
-      switch (toggleName)
-      {
-        case "green_tgr":
-          return 1;
-        case "l_green_tgr":
-          return 2;
-        case "yellow_tgr":
-          return 3;
-        case "orange_tgr":
-          return 4;
-        case "red_tgr":
-          return 5;
-        default:
-          return 0;
-      }
-    }
-  }
-
 }
