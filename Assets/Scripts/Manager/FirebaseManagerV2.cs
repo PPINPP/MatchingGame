d7f39ae4ec694551e3c47f5f31a3cbb7daafc698
PINPIN <lwarissara.p@gmail.com>
2025-09-03 15:18:34 +0700

    Update Qvalue & Collection name
    - edit Qvalue in Qtable
    - change firebase collection name to be qlearning_kmutt

--- Assets/Scripts/Manager/FirebaseManagerV2.cs
+++ Assets/Scripts/Manager/FirebaseManagerV2.cs
@@ -39,7 +39,7 @@ public class FirebaseManagerV2 : MonoSingleton<FirebaseManagerV2>
     /// Profile Parameter ///
     private bool syncnetwork = true;
     string curr_id;
-    string prefix_locate = "q_test";
+    string prefix_locate = "qlearning_kmutt";
     // string prefix_locate = "Debug";
     string prefix_time_locate = "game_information_test";
     public string curr_username;
